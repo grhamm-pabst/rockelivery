31428ac0aa47328cfb79ba87900da9ca4a21913d
grhamm-pabst <grhammpabst456@gmail.com>
2021-05-13 13:19:14 -0300

    Format change

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -12,6 +12,8 @@ defmodule Rockelivery do
   defdelegate delete_user(id), to: UserDelete, as: :call
   defdelegate update_user(params), to: UserUpdate, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
+
   defdelegate create_item(params), to: ItemCreate, as: :call
+
   defdelegate create_order(params), to: OrderCreate, as: :call
 end
