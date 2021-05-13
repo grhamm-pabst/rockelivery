aa5a33d5d2909249a71744eedc5ba20adda8c1ed
grhamm-pabst <grhammpabst456@gmail.com>
2021-05-13 13:17:06 -0300

    Facade adition of create_order

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,6 +1,8 @@
 defmodule Rockelivery do
   alias Rockelivery.Items.Create, as: ItemCreate
 
+  alias Rockelivery.Orders.Create, as: OrderCreate
+
   alias Rockelivery.Users.Create, as: UserCreate
   alias Rockelivery.Users.Delete, as: UserDelete
   alias Rockelivery.Users.Get, as: UserGet
@@ -11,4 +13,5 @@ defmodule Rockelivery do
   defdelegate update_user(params), to: UserUpdate, as: :call
   defdelegate get_user_by_id(id), to: UserGet, as: :by_id
   defdelegate create_item(params), to: ItemCreate, as: :call
+  defdelegate create_order(params), to: OrderCreate, as: :call
 end
